4dea9eb024a3b56391cfd91515873de09df3d80e
Sam Tebbs <samuel.tebbs@gmail.com>
2018-02-17 13:44:22 +0000

    Added Serialisation

---
+++ src/main/java/com/teamness/smane/Serialisation.java
@@ -0,0 +1,23 @@
+package com.teamness.smane;
+
+import java.io.*;
+import java.util.Base64;
+
+public class Serialisation {
+
+    public Object toObject(String str) throws IOException, ClassNotFoundException {
+        byte[] bytes = Base64.getDecoder().decode(str);
+        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
+            return ois.readObject();
+        }
+    }
+
+    public String fromObject(Serializable obj) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try(ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(obj);
+            return Base64.getEncoder().encodeToString(baos.toByteArray());
+        }
+    }
+
+}
