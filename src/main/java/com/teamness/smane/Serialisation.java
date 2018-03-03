4b092615df74dc2a5800c96c86066d048992a31e
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-03 15:05:10 +0000

    Made Serialisation use non-java 8 things

--- src/main/java/com/teamness/smane/Serialisation.java
+++ src/main/java/com/teamness/smane/Serialisation.java
@@ -1,23 +1,39 @@
 package com.teamness.smane;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.*;
 import java.util.Base64;
 
 public class Serialisation {
 
     public static Object toObject(String str) throws IOException, ClassNotFoundException {
-        byte[] bytes = Base64.getDecoder().decode(str);
+        byte[] bytes = DatatypeConverter.parseBase64Binary(str);
+        return deserialiseBytes(bytes);
+    }
+
+    private static Object deserialiseBytes(byte[] bytes) throws ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
             return ois.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 
     public static String fromObject(Serializable obj) throws IOException {
+        byte[] serialised = serialiseObject(obj);
+        return DatatypeConverter.printBase64Binary(serialised);
+    }
+
+    public static byte[] serialiseObject(Serializable obj) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try(ObjectOutputStream oos = new ObjectOutputStream(baos)) {
             oos.writeObject(obj);
-            return Base64.getEncoder().encodeToString(baos.toByteArray());
+            return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 
 }
