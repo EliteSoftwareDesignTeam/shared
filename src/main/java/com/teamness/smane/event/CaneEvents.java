276b415951cd52b271af6d07d56fa3a06995d41a
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-03 11:51:53 +0000

    Removed CaneEvents.ALL and refactored CaneEvents.BT to BT_OUT and BT_IN

--- src/main/java/com/teamness/smane/event/CaneEvents.java
+++ src/main/java/com/teamness/smane/event/CaneEvents.java
@@ -2,6 +2,6 @@ package com.teamness.smane.event;
 
 public class CaneEvents {
 
-    public static final EventChannel LOCAL = new EventChannel(), BT = new EventChannel(), ALL = new EventChannel(EventChannel.EventPriority.MEDIUM, EventChannel.EventPriority.HIGH, LOCAL, BT);
+    public static final EventChannel LOCAL = new EventChannel(), BT_OUT = new EventChannel(), BT_IN = new EventChannel();
 
 }
