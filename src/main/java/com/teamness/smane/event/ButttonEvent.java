ec1db0abdf308dc537a76c819c529dfe090f7037
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-10 17:51:59 +0000

    Added ButtonEvent

--- src/main/java/com/teamness/smane/event/ButttonEvent.java
+++ src/main/java/com/teamness/smane/event/ButttonEvent.java
@@ -2,6 +2,12 @@ package com.teamness.smane.event;
 
 public class ButttonEvent {
 
+    public final ButtonAction action;
+
+    public ButttonEvent(ButtonAction action) {
+        this.action = action;
+    }
+
     public static enum ButtonAction {
         PRESSED, RELEASED
     }
