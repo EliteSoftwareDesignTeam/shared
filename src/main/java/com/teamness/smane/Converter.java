38d46c434cf828dfb621e30287182bbae9b72292
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-19 12:21:47 +0000

    Added Converter.identity

--- src/main/java/com/teamness/smane/Converter.java
+++ src/main/java/com/teamness/smane/Converter.java
@@ -4,4 +4,11 @@ public interface Converter<F, T> {
 
     T convert(F t);
 
+    static Converter<?, ?> identity = new Converter<Object, Object>() {
+        @Override
+        public Object convert(Object t) {
+            return t;
+        }
+    };
+
 }
