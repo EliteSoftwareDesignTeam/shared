c21db8d5222e62d7d95d2e035866d28a1c8b4f63
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-23 14:15:00 +0000

    Removed Converter.identity()

--- src/main/java/com/teamness/smane/Converter.java
+++ src/main/java/com/teamness/smane/Converter.java
@@ -4,13 +4,4 @@ public interface Converter<F, T> {
 
     T convert(F t);
 
-    static <T> Converter<T, T> identity() {
-        return new Converter<T, T>() {
-            @Override
-            public T convert(T t) {
-                return t;
-            }
-        };
-    }
-
 }
